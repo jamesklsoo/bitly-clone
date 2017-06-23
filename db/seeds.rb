44161c52cf60c92e8e4bc35681db874c5799bf57
James Soo <jamesklsoo@gmail.com>
2017-06-23 14:15:26 +0800

    commented

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-require_relative '../app/models/url.rb'
-require 'securerandom'
+# require_relative '../app/models/url.rb'
+# require 'securerandom'
 
-s = ''
+# s = ''
 
-File.open("db/urls.csv", "r").each_line do |line|
-  s << "('#{line[1..-4]}', '#{SecureRandom.hex(6)}', 0)#{line[-2]}"
-end
+# File.open("db/urls.csv", "r").each_line do |line|
+#   s << "('#{line[1..-4]}', '#{SecureRandom.hex(6)}', 0)#{line[-2]}"
+# end
 
-sql = "INSERT INTO urls (long_url, short_url, click_count) VALUES #{s}"
+# sql = "INSERT INTO urls (long_url, short_url, click_count) VALUES #{s}"
 
-Url.transaction do
-  Url.connection.execute sql
-end
+# Url.transaction do
+#   Url.connection.execute sql
+# end
